11340f6c1ca30ba7eee42ed944551f67d5bfc9a0
Ibrahim Al Shukri <alshukri511@gmail.com>
2018-10-29 22:40:20 +0400

    Add submit for height and width

--- designs.js
+++ designs.js
@@ -3,6 +3,13 @@
 var height, width, color 
 
 // When size is submitted by the user, call makeGrid()
+$('#sizePicker').submit(function (event)  {
+    event.preventDefault();
+    height = $('#inputHeight').val();
+    width = $('inputWidth').val();
+    makeGrid(height, width);
+})
+
 
 function makeGrid() {
 
