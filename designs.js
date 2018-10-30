3cdd9ba9f57abc3c8a04d243b52667e0fd4beb38
Ibrahim Al Shukri <alshukri511@gmail.com>
2018-10-30 22:23:09 +0400

    Fix: GetElementById was defined wrongly

--- designs.js
+++ designs.js
@@ -1,23 +1,23 @@
 // Select color input
 // Select size input
-var height, width, color 
+var height, width, color
 
 // When size is submitted by the user, call makeGrid()
-$('#sizePicker').submit(function (event)  {
+const gridSize = document.getElementById("sizePicker");
+gridSize.addEventListener('submit', function (event)  {
     event.preventDefault();
     height = $('#inputHeight').val();
     width = $('#inputWidth').val();
     makeGrid(height, width);
-    // check if function reads user input
+    // check if function reads user input in console
     // console.log('height: ' + height + 'width: ' + width)
 })
 
 
 function makeGrid(a, b) {
+    // remove old table
     $('tr').remove();
-    // Your code goes here!
-    // to make a table:
-    // creates a table of n x m
+    // TODO: creates a table of n x m
     for (var n = 1; n <= a; n++) {
         $('#pixelCanvas').append('<tr id=table' + n + '></tr>');
         for (var m =1; m <= b; m++) {
@@ -25,11 +25,11 @@ function makeGrid(a, b) {
         }
     }
 
-    // to add color:
-    // code assigns a color from user choice then applies to the grid
-    // adds color if grid block is un-colored and removes color if grid block is colored.
+
+
+    // TODO: EventListener assigns a color if grid block is un-colored and removes color if grid block is colored
     $('td').click(function PickColor() {
-        color = $('#colorPicker').val(); 
+        var color = $('#colorPicker').val(); 
         if ($(this).attr('style')) {
             $(this).removeAttr('style')
         } else {
